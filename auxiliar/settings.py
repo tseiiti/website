fa7ff5f1797e159b03bbb9849c58c425ebf453cb
oscar.taniguchi <oscar.taniguchi@tecnogroup.com.br>
2024-02-17 18:03:17 -0300

    configurando banco postgres

--- auxiliar/settings.py
+++ auxiliar/settings.py
@@ -63,12 +63,15 @@ WSGI_APPLICATION = 'auxiliar.wsgi.app'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
-  # 'default': {
-  #   'ENGINE': 'django.db.backends.sqlite3',
-  #   'NAME': BASE_DIR / 'db.sqlite3',
-  # }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.getenv('POSTGRES_DATABASE'),
+    'USER': os.getenv('POSTGRES_USER'),
+    'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+    'HOST': os.getenv('POSTGRES_HOST'),
+    'PORT': os.getenv('POSTGRES_DB_PORT'),
+  }
 }
 
 
--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 echo "**********     início     **********"
+
+
 echo "**********  pip install   **********"
 python3 -m pip install -r requirements.txt
+
 echo "********** makemigrations **********"
 python3 manage.py makemigrations --noinput
+
 echo "**********    migrate     **********"
 python3 manage.py migrate --noinput
+
 echo "********** collectstatic  **********"
 python3 manage.py collectstatic --noinput
+
+
 echo "**********      fim       **********"
