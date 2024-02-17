93de01ec33a4430149181ef872b42696c06136fd
oscar.taniguchi <oscar.taniguchi@tecnogroup.com.br>
2024-02-16 21:34:29 -0300

    testando variáveis de ambiente

--- _website/settings.py
+++ _website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6=*m)bkqd0)!b*k3)whx14bhc@1^aqr(_kt!m*3yl&x85)+s8k'
+SECRET_KEY = os.environ["SECRET_KEY"] if "SECRET_KEY" in os.environ.keys() else "django-insecure-django-insecure-django-insecure-django-insecure-dj"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
