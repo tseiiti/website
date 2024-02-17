6f8b7c6d60a542695c533936175024e0f51bfeb0
oscar.taniguchi <oscar.taniguchi@tecnogroup.com.br>
2024-02-17 16:33:33 -0300

    funcionando home com statics, ainda falta o banco

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-echo "******** inicio ********"
+echo "**********     início     **********"
+echo "**********  pip install   **********"
 python3 -m pip install -r requirements.txt
+echo "********** makemigrations **********"
 python3 manage.py makemigrations --noinput
+echo "**********    migrate     **********"
 python3 manage.py migrate --noinput
+echo "********** collectstatic  **********"
 python3 manage.py collectstatic --noinput
-echo "********  fim   ********"
+echo "**********      fim       **********"
