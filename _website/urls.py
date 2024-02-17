82df44fc1d536b3cdb2140103467b13d613afed0
oscar.taniguchi <oscar.taniguchi@tecnogroup.com.br>
2024-02-16 22:31:39 -0300

    troca de home por index

--- _website/urls.py
+++ _website/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from website import views
 
 urlpatterns = [
-  path("", views.home, name="home"),
+  path("", views.index, name="index"),
+  path("home", views.home, name="home"),
   path("signup", views.signup, name="signup"),
   path("signin", views.signin, name="signin"),
   path("signout", views.signout, name="signout"),
--- website/views.py
+++ website/views.py
@@ -5,31 +5,21 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from datetime import datetime
 from .forms import UserForm
 
-def index(request):
-  now = datetime.now()
-  html = f'''
-  <html>
-    <body>
-      <h1>Hello from Vercel!</h1>
-      <p>The current time is { now }.</p>
-    </body>
-  </html>
-  '''
-  return HttpResponse(html)
-
 def home(request):
+  return render(request, "home.html")
+
+def index(request):
   is_authenticated = request.user.is_authenticated
   if is_authenticated:
-    return render(request, "home.html", { "title": "Home", "sidenav": settings.SIDENAV, "is_authenticated": is_authenticated })
+    return render(request, "index.html", { "title": "Home", "sidenav": settings.SIDENAV, "is_authenticated": is_authenticated })
   else:
-    return render(request, "home_external.html", { "title": "Home" })
+    return render(request, "index_external.html", { "title": "Home" })
 
 def signup(request):
   if request.method == "GET":
-    return render(request, "app/signup.html", { "form": UserForm })
+    return render(request, "signup.html", { "form": UserForm })
   else:
     username = request.POST.get("username")
     email = request.POST.get("email")
@@ -41,14 +31,14 @@ def signup(request):
     if password == "": 
       # form = UserForm(request.POST)
       # raise form.ValidationError("This error was added to show the non field errors styling.")
-      return render(request, "app/signup.html", { "errors": [{ "field": "password", "message": "Senha inválida" }], "form": UserForm(request.POST) })
+      return render(request, "signup.html", { "errors": [{ "field": "password", "message": "Senha inválida" }], "form": UserForm(request.POST) })
     if password != confirm:
-      return render(request, "app/signup.html", { "errors": [{ "field": "confirm", "message": "Senha não confirma" }], "form": UserForm(request.POST) })
+      return render(request, "signup.html", { "errors": [{ "field": "confirm", "message": "Senha não confirma" }], "form": UserForm(request.POST) })
     if username == "":
-      return render(request, "app/signup.html", { "errors": [{ "field": "username", "message": "Usuário inválido" }], "form": UserForm(request.POST) })
+      return render(request, "signup.html", { "errors": [{ "field": "username", "message": "Usuário inválido" }], "form": UserForm(request.POST) })
     user = User.objects.filter(username=username).first()
     if user:
-      return render(request, "app/signup.html", { "errors": [{ "field": "username", "message": "Usuário já cadastrado" }], "form": UserForm(request.POST) })
+      return render(request, "signup.html", { "errors": [{ "field": "username", "message": "Usuário já cadastrado" }], "form": UserForm(request.POST) })
     
     user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
     user = authenticate(username=username, password=password)
@@ -57,7 +47,7 @@ def signup(request):
 
 def signin(request):
   if request.method == "GET":
-    return render(request, "app/signin.html")
+    return render(request, "signin.html")
   else:
     username = request.POST.get("username")
     password = request.POST.get("password")
@@ -66,12 +56,12 @@ def signin(request):
       login(request, user)
       reverse_lazy("modelo:list")
     else:
-      return render(request, "app/signin.html", { "errors": [{ "field": "username", "message": "Usuário/Senha inválido" }]})
+      return render(request, "signin.html", { "errors": [{ "field": "username", "message": "Usuário/Senha inválido" }]})
 
 @login_required(login_url="")
 def signout(request):
   if request.method == "GET":
-    return render(request, "app/signout.html")
+    return render(request, "signout.html")
   else:
     logout(request)
     return HttpResponse("Logout")
