510da7beac4df9fbde741ecaa64d207aa6a475a2
oscar.taniguchi <oscar.taniguchi@tecnogroup.com.br>
2024-02-17 15:02:37 -0300

    corrigindo noinput

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
 echo "******** inicio ********"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noimput --clear
+python3 -m pip install -r requirements.txt
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
 echo "********  fim   ********"
