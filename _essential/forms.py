8fd9ab9873e1b5ba6e76d2af0537aaf13d72f3c4
oscar.taniguchi <oscar.taniguchi@tecnogroup.com.br>
2024-02-18 14:05:19 -0300

    login funcionando

--- _essential/forms.py
+++ _essential/forms.py
@@ -2,18 +2,52 @@ from django import forms
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 
-class UserForm(forms.Form):
-  username = forms.CharField(label="Usuário", max_length=255)
-  email = forms.EmailField(label="E-mail", help_text="", max_length=255, widget=forms.TextInput(attrs={ "placeholder": "ex.: name@example.com" }))
-  first_name = forms.CharField(label="Nome", max_length=255)
-  last_name = forms.CharField(label="Sobrenome", max_length=255)
-  password = forms.CharField(label="Senha", widget=forms.PasswordInput, min_length=3)
-  confirm = forms.CharField(label="Confirme a senha", widget=forms.PasswordInput, help_text="<i>Reescreva igualmente a senha para confirmação</i>")
-
+class SignUpForm(forms.Form):
+  username = forms.CharField(
+    label = "Usuário", 
+    max_length=255
+  )
+  email = forms.EmailField(
+    label = "E-mail", 
+    help_text = "", 
+    max_length = 255, 
+    widget = forms.TextInput(
+      attrs = { "placeholder": "ex.: name@example.com" }
+    )
+  )
+  first_name = forms.CharField(
+    label = "Nome", 
+    max_length = 255
+  )
+  last_name = forms.CharField(
+    label = "Sobrenome", 
+    max_length = 255
+  )
+  password = forms.CharField(
+    label = "Senha", 
+    widget = forms.PasswordInput, 
+    min_length = 3
+  )
+  confirm = forms.CharField(
+    label = "Confirme a senha", 
+    widget = forms.PasswordInput, 
+    help_text = "<i>Reescreva igualmente a senha para confirmação</i>"
+  )
+  
   def clean(self):
     cd = super().clean()
     if cd.get("password") != cd.get("confirm"):
       self.add_error("confirm", "Senha não confirma")
-    if User.objects.filter(username=cd.get("username")).first():
+    elif User.objects.filter(username=cd.get("username")).first():
       self.add_error("username", "Usuário já cadastrado")
     return cd
+
+class SignInForm(forms.Form):
+  username = forms.CharField(
+    label = "Usuário", 
+    max_length=255
+  )
+  password = forms.CharField(
+    label = "Senha", 
+    widget = forms.PasswordInput
+  )
--- _essential/views.py
+++ _essential/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
-from django.urls import reverse_lazy, reverse
-from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .forms import UserForm
+from .forms import SignUpForm, SignInForm
 
 def index(request):
 	is_authenticated = request.user.is_authenticated
@@ -16,40 +16,49 @@ def index(request):
 
 def signup(request):
 	if request.method == "GET":
-		return render(request, "signup.html", { "form": UserForm })
+		return render(request, "signup.html", { "form": SignUpForm })
 	else:
-		form = UserForm(request.POST)
+		post = request.POST
+		form = SignUpForm(post)
 		if form.is_valid():
 			# messages.success(request, 'You have successfully updated the status from open to Close')
 			user = User.objects.create_user(
-				username = request.POST.get("username"),
-				email = request.POST.get("email"),
-				first_name = request.POST.get("first_name"),
-				last_name = request.POST.get("last_name"),
-				password = request.POST.get("password")
+				username = post.get("username"),
+				email = post.get("email"),
+				first_name = post.get("first_name"),
+				last_name = post.get("last_name"),
+				password = post.get("password")
 			)
 			user = authenticate(
-				username=request.POST.get("username"),
-				password=request.POST.get("password")
+				username=post.get("username"),
+				password=post.get("password")
 			)
 			login(request, user)
 			return HttpResponseRedirect(reverse("example:list"))
 		else:
 			form.add_error(None, "Erro(s) foram encontrado(s) ao criar a conta. Favor fazer a devidas correções e tentar novamente.")
-			return render(request, "signup.html", { "form": form})
+			return render(request, "signup.html", { "form": form })
 
 def signin(request):
 	if request.method == "GET":
-		return render(request, "signin.html")
+		return render(request, "signin.html", { "form": SignInForm })
 	else:
 		username = request.POST.get("username")
 		password = request.POST.get("password")
 		user = authenticate(username=username, password=password)
 		if user:
 			login(request, user)
-			reverse_lazy("example:list")
+			return HttpResponseRedirect(reverse("example:list"))
 		else:
-			return render(request, "signin.html", { "errors": [{ "field": "username", "message": "Usuário/Senha inválido" }]})
+			post = request.POST
+			form = SignInForm(post)
+			# form = SignInForm()
+			# form.is_valid()
+			form.add_error(None, "Usuário / Senha inválido")
+			form.add_error("password", "")
+			form.add_error("username", "")
+			return render(request, "signin.html", { "form": form })
+			# return render(request, "signin.html", { "errors": [{ "field": "username", "message": "Usuário/Senha inválido" }]})
 
 @login_required(login_url="")
 def signout(request):
