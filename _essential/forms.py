acfba9c586e75bd550f09fbdce3269c35ab66369
oscar.taniguchi <oscar.taniguchi@tecnogroup.com.br>
2024-02-17 21:12:24 -0300

    signup funcionando

--- _essential/forms.py
+++ _essential/forms.py
@@ -1,27 +1,19 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 
 class UserForm(forms.Form):
   username = forms.CharField(label="Usuário", max_length=255)
   email = forms.EmailField(label="E-mail", help_text="", max_length=255, widget=forms.TextInput(attrs={ "placeholder": "ex.: name@example.com" }))
   first_name = forms.CharField(label="Nome", max_length=255)
   last_name = forms.CharField(label="Sobrenome", max_length=255)
-  password = forms.CharField(label="Senha", widget=forms.PasswordInput)
+  password = forms.CharField(label="Senha", widget=forms.PasswordInput, min_length=3)
   confirm = forms.CharField(label="Confirme a senha", widget=forms.PasswordInput, help_text="<i>Reescreva igualmente a senha para confirmação</i>")
 
-  # def clean(self):
-  #   cleaned_data = super().clean()
-  #   raise forms.ValidationError("This error was added to show the non field errors styling.")
-  #   return cleaned_data
-  
-  # def clean(self):
-  #     cleaned_data = super().clean()
-  #     cc_myself = cleaned_data.get("cc_myself")
-  #     subject = cleaned_data.get("subject")
-
-  #     if cc_myself and subject:
-  #         # Only do something if both fields are valid so far.
-  #         if "help" not in subject:
-  #             raise ValidationError(
-  #                 "Did not send for 'help' in the subject despite "
-  #                 "CC'ing yourself."
-  #             )
+  def clean(self):
+    cd = super().clean()
+    if cd.get("password") != cd.get("confirm"):
+      self.add_error("confirm", "Senha não confirma")
+    if User.objects.filter(username=cd.get("username")).first():
+      self.add_error("username", "Usuário já cadastrado")
+    return cd
--- _essential/views.py
+++ _essential/views.py
@@ -1,67 +1,60 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
-from django.urls import reverse_lazy
-from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .forms import UserForm
 
-def home(request):
-  return render(request, "home.html")
-
 def index(request):
-  is_authenticated = request.user.is_authenticated
-  if is_authenticated:
-    return render(request, "index.html", { "title": "Home", "sidenav": settings.SIDENAV, "is_authenticated": is_authenticated })
-  else:
-    return render(request, "index_external.html", { "title": "Home" })
+	is_authenticated = request.user.is_authenticated
+	if is_authenticated:
+		return render(request, "index.html", { "title": "Home", "sidenav": settings.SIDENAV, "is_authenticated": is_authenticated })
+	else:
+		return render(request, "index_external.html", { "title": "Home" })
 
 def signup(request):
-  if request.method == "GET":
-    return render(request, "signup.html", { "form": UserForm })
-  else:
-    username = request.POST.get("username")
-    email = request.POST.get("email")
-    first_name = request.POST.get("first_name")
-    last_name = request.POST.get("last_name")
-    password = request.POST.get("password")
-    confirm = request.POST.get("confirm")
-    # return HttpResponse(dict(request))
-    if password == "": 
-      # form = UserForm(request.POST)
-      # raise form.ValidationError("This error was added to show the non field errors styling.")
-      return render(request, "signup.html", { "errors": [{ "field": "password", "message": "Senha inválida" }], "form": UserForm(request.POST) })
-    if password != confirm:
-      return render(request, "signup.html", { "errors": [{ "field": "confirm", "message": "Senha não confirma" }], "form": UserForm(request.POST) })
-    if username == "":
-      return render(request, "signup.html", { "errors": [{ "field": "username", "message": "Usuário inválido" }], "form": UserForm(request.POST) })
-    user = User.objects.filter(username=username).first()
-    if user:
-      return render(request, "signup.html", { "errors": [{ "field": "username", "message": "Usuário já cadastrado" }], "form": UserForm(request.POST) })
-    
-    user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
-    user = authenticate(username=username, password=password)
-    login(request, user)
-    reverse_lazy("example:list")
+	if request.method == "GET":
+		return render(request, "signup.html", { "form": UserForm })
+	else:
+		form = UserForm(request.POST)
+		if form.is_valid():
+			# messages.success(request, 'You have successfully updated the status from open to Close')
+			user = User.objects.create_user(
+				username = request.POST.get("username"),
+				email = request.POST.get("email"),
+				first_name = request.POST.get("first_name"),
+				last_name = request.POST.get("last_name"),
+				password = request.POST.get("password")
+			)
+			user = authenticate(
+				username=request.POST.get("username"),
+				password=request.POST.get("password")
+			)
+			login(request, user)
+			return HttpResponseRedirect(reverse("example:list"))
+		else:
+			form.add_error(None, "Erro(s) foram encontrado(s) ao criar a conta. Favor fazer a devidas correções e tentar novamente.")
+			return render(request, "signup.html", { "form": form})
 
 def signin(request):
-  if request.method == "GET":
-    return render(request, "signin.html")
-  else:
-    username = request.POST.get("username")
-    password = request.POST.get("password")
-    user = authenticate(username=username, password=password)
-    if user:
-      login(request, user)
-      reverse_lazy("example:list")
-    else:
-      return render(request, "signin.html", { "errors": [{ "field": "username", "message": "Usuário/Senha inválido" }]})
+	if request.method == "GET":
+		return render(request, "signin.html")
+	else:
+		username = request.POST.get("username")
+		password = request.POST.get("password")
+		user = authenticate(username=username, password=password)
+		if user:
+			login(request, user)
+			reverse_lazy("example:list")
+		else:
+			return render(request, "signin.html", { "errors": [{ "field": "username", "message": "Usuário/Senha inválido" }]})
 
 @login_required(login_url="")
 def signout(request):
-  if request.method == "GET":
-    return render(request, "signout.html")
-  else:
-    logout(request)
-    return HttpResponse("Logout")
+	if request.method == "GET":
+		return render(request, "signout.html")
+	else:
+		logout(request)
+		return HttpResponse("Logout")
