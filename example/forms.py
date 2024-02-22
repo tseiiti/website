c871442b31c221207db6e1c6be5fe173c72874de
oscar.taniguchi <oscar.taniguchi@tecnogroup.com.br>
2024-02-22 10:06:44 -0300

    commit temporário

--- example/forms.py
+++ example/forms.py
@@ -1,7 +1,77 @@
 from django import forms
-from .models import Pessoa
+# from django.forms import ModelForm, inlineformset_factory, formset_factory
+from .models import Pessoa, Filho
 
-class PessoaForm(forms.ModelForm):
+from django.views.generic.base import ContextMixin
+from django.conf import settings
+import re
+
+class MyContext(ContextMixin):
+	def get_title(self):
+		title = None
+		sn = self.__class__.__name__
+		sn = re.sub(r'List$', '', sn)
+		sn = re.sub(r'Create$', '', sn)
+		sn = re.sub(r'Update$', '', sn)
+		sn = re.sub(r'Detail$', '', sn)
+		sn = re.sub(r'Delete$', '', sn)
+		bn = self.__class__.__bases__[1].__name__
+		if   bn == "ListView":   title = "Listar " + sn
+		elif bn == "CreateView": title = "Criar " + sn
+		elif bn == "UpdateView": title = "Atualizar " + sn
+		elif bn == "DetailView": title = "Visualizar " + sn
+		elif bn == "DeleteView": title = "Excluir " + sn
+		return title
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context["title"] = self.get_title()
+		context["sidenav"] = settings.SIDENAV
+		context["user"] = self.request.user
+		return context
+
+class PessoaForm(ContextMixin, forms.ModelForm):
   class Meta:
     model = Pessoa
     fields = '__all__'
+
+class FilhoForm(ContextMixin, forms.ModelForm):
+  class Meta:
+    model = Filho
+    # fields = '__all__'
+    fields = ('nome', 'dta_nasc')
+
+# FilhoFormSet = inlineformset_factory(Pessoa, Filho, fields='__all__')
+FilhoFormSet = forms.formset_factory(FilhoForm, extra=3)
+
+# formset = FilhoFormSet(
+#   initial = [
+#     { "nome": "teste", "dta_nasc": datetime.date.today() }
+# 	]
+# )
+
+
+
+# (
+#   parent_model: type[Model], 
+#   model: type[Model], 
+#   form: type[ModelForm] = ..., 
+#   formset: type[BaseInlineFormSet] = ..., 
+#   fk_name: str | None = ..., 
+#   fields: _Fields | None = ..., 
+#   exclude: _Fields | None = ..., 
+#   extra: int = ..., 
+#   can_order: bool = ..., 
+#   can_delete: bool = ..., 
+#   max_num: int | None = ..., 
+#   formfield_callback: ((...) -> Any) | None = ..., 
+#   widgets: dict[str, Any] | None = ..., 
+#   validate_max: bool = ..., 
+#   localized_fields: Sequence[str] | None = ..., 
+#   labels: dict[str, str] | None = ..., 
+#   help_texts: dict[str, str] | None = ..., 
+#   error_messages: dict[str, dict[str, str]] | None = ..., 
+#   min_num: int | None = ..., 
+#   validate_min: bool = ..., 
+#   field_classes: dict[str, Any] | None = ...
+# ) -> type[BaseInlineFormSet]
--- example/views.py
+++ example/views.py
@@ -1,25 +1,80 @@
 # from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 from django.urls import reverse_lazy
-from .models import MyContext, Pessoa
+from django.db import transaction
 
-class PessoaList(MyContext, ListView):
+from .models import Pessoa
+from .forms import PessoaForm, FilhoFormSet
+
+class CreatePessoaView(CreateView):
+	template_name = 'example/create_pessoa.html'
+	model = Pessoa
+	form_class = PessoaForm
+	success_url = './'
+
+	# def get_context_data(self, **kwargs):
+	# 	context = super().get_context_data(**kwargs)
+	# 	if self.request.POST:
+	# 		context['formset'] = FilhoFormSet(self.request.POST, self.request.FILES, instance=self.object)
+	# 	else:
+	# 		context['formset'] = FilhoFormSet(instance=self.object)
+	# 	return context
+	
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['formset'] = FilhoFormSet(initial=self.object)
+		return context
+	
+
+# formset = FilhoFormSet(
+#   initial = [
+#     { "nome": "teste", "dta_nasc": datetime.date.today() }
+# 	]
+# )
+
+	def form_valid(self, form, formset):
+		with transaction.atomic():
+			self.object = form.save()
+
+		if formset.is_valid():
+			formset.instance = self.object
+			formset.save()
+
+		return super().form_valid(form)
+
+	def post(self, request, *args, **kwargs):
+		self.object = None
+		form_class = self.get_form_class()
+		form = self.get_form(form_class)
+		formset = FilhoFormSet(self.request.POST, self.request.FILES)
+		if form.is_valid() and formset.is_valid():
+			return self.form_valid(form, formset)
+		else:
+			return self.form_invalid(form, formset)
+
+	def form_invalid(self, form, formset):
+		return self.render_to_response(self.get_context_data(form=form, formset=formset))
+
+
+
+
+class PessoaList(ListView):
 	model = Pessoa
 	queryset = Pessoa.objects.all()
 
-class PessoaCreate(MyContext, CreateView):
+class PessoaCreate(CreateView):
 	model = Pessoa
 	fields = "__all__"
 	success_url = reverse_lazy("example:list")
 
-class PessoaUpdate(MyContext, UpdateView):
+class PessoaUpdate(UpdateView):
 	model = Pessoa
 	fields = "__all__"
 	success_url = reverse_lazy("example:list")
 
-class PessoaDetail(MyContext, DetailView):
+class PessoaDetail(DetailView):
 	queryset = Pessoa.objects.all()
 
-class PessoaDelete(MyContext, DeleteView):
+class PessoaDelete(DeleteView):
 	queryset = Pessoa.objects.all()
 	success_url = reverse_lazy("example:list")
