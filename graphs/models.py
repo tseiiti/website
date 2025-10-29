5df3f570ed625ea5f6d2d8522edc5cbd634aa000
Seiiti <seiiti@popos.com>
2025-10-29 18:15:40 -0300

    camada e memória

--- graphs/models.py
+++ graphs/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 class Temp(models.Model):
   ip = models.CharField(max_length=15)
+  camada = models.IntegerField(null=True)
   pai = models.CharField(max_length=17)
   endereco = models.CharField(max_length=17)
   temperatura = models.DecimalField(max_digits=5, decimal_places=2)
+  memoria = models.IntegerField(null=True)
   data_hora = models.DateTimeField(auto_now=True)
   
   def __str__(self):
