5acab89e1585e949adb26d2c9bea600b30137f47
Seiiti <seiiti@popos.com>
2025-11-10 17:46:47 -0300

    ajuste

--- _core/wsgi.py
+++ _core/wsgi.py
@@ -4,4 +4,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_core.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+
+app = application
