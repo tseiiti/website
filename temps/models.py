20a373e8e385473abf430e082f977915a5c095f0
seiiti <tseiiti@gmail.com>
2025-11-28 06:37:52 -0300

    novos campos iot

--- temps/models.py
+++ temps/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 class Temp(models.Model):
+  nome = models.CharField(max_length=15, null=True)
   ip = models.CharField(max_length=15)
   camada = models.IntegerField(null=True)
   pai = models.CharField(max_length=17)
   endereco = models.CharField(max_length=17)
   temperatura = models.DecimalField(max_digits=5, decimal_places=2)
+  peso = models.DecimalField(max_digits=5, decimal_places=2, null=True)
   memoria = models.IntegerField(null=True)
   data_hora = models.DateTimeField(auto_now=True)
   
